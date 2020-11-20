ba009f60c2e1ef4b4d88e75ecb7d09561ca67b1d
eflipe <felipecabaleiro@gmail.com>
2020-11-19 21:45:08 -0300

    +keys

--- models/constants.py
+++ models/constants.py
@@ -5,6 +5,6 @@ import os
 # API_KEY_HERE = 'DTefltZHcbSfUzgnavsqWrZbsPZgEJ3EFHyz-EDcqBc'
 # API_IP_TOKEN = "0b334fd91614f7"
 
-URL_HERE = os.getenv("URL_HERE")
-API_KEY_HERE = os.getenv("API_KEY_HERE")
+URL_HERE = os.environ.get("URL_HERE")
+API_KEY_HERE = os.environ.get("API_KEY_HERE")
 API_IP_TOKEN = os.getenv("API_IP_TOKEN")
