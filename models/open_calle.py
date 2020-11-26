f247d9c1ff842ba28dcdd0fa41500740215f7d80
eflipe <felipecabaleiro@gmail.com>
2020-11-26 13:45:01 -0300

    + Vamo'

--- models/open_calle.py
+++ models/open_calle.py
@@ -1,13 +1,11 @@
 import io
 
 
-file_txt = 'calles_text.txt'
-nuevo_text = []
-nuevo_p = []
-
-
 # ./calles_text.txt
-def text_to_list(file_txt=None):
+def text_to_list():
+    file_txt = 'calles_text.txt'
+    nuevo_text = []
+    nuevo_p = []
     with io.open(file_txt) as openfile:
         for line in openfile:
             nuevo_text.append(line.rstrip('\n'))
