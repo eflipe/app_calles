120bdf0d20df1374ed6a09933f213c7af4cc0c34
eflipe <felipecabaleiro@gmail.com>
2020-11-26 13:25:13 -0300

    - Buscador de info desde un txt. Spaghetti code

--- models/open_calle.py
+++ models/open_calle.py
@@ -7,7 +7,7 @@ nuevo_p = []
 
 
 # ./calles_text.txt
-def text_to_list(file_text=None):
+def text_to_list(file_txt=None):
     with io.open(file_txt) as openfile:
         for line in openfile:
             nuevo_text.append(line.rstrip('\n'))
