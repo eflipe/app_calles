389eedfaa05faadd397291e34b7b4f37add94796
eflipe <felipecabaleiro@gmail.com>
2020-11-27 01:02:20 -0300

    + Vamo'

--- models/api_calle.py
+++ models/api_calle.py
@@ -8,5 +8,8 @@ def geolocator(lat, long):
     coordinates = f"{lat}, {long}"
     location = locator.reverse(coordinates)
     road_string = location.raw['address']['road']
+    if 'Doctor' in road_string
+        long = len('Doctor')
+        return road_string[long:]
     print(road_string)
     return road_string
