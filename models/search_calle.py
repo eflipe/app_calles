7d7de16e7cb9d430615a36a4da54d6d68c6dbb09
eflipe <felipecabaleiro@gmail.com>
2020-11-27 00:49:23 -0300

    + Vamo'

--- models/search_calle.py
+++ models/search_calle.py
@@ -52,6 +52,8 @@ def calle_txt(nombre_calle=None):
     calle_info = []
     print(nombre_calle)
     nombre_calle = (' ').join(nombre_calle.split('_'))
+    print('Despues Split', nombre_calle)
+
     for linea in nuevo_nuevo:
         index = 0
         prev = 0
