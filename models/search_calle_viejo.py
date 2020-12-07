0244c0cdf3137baf2e1a1d6f9f81b5fa2125aa86
eflipe <felipecabaleiro@gmail.com>
2020-12-06 21:02:39 -0300

    Nuevo text

--- models/search_calle_viejo.py
+++ models/search_calle_viejo.py
@@ -8,21 +8,23 @@ print(basedir)
 ruta = os.path.join(basedir, file_txt)
 print(ruta)
 
+# nombre_calle = "Gorriti"
+# nombre_calle = "CRÁMER"
+# nombre_calle = "corrientes"
+# print(nuevo_text)
+
 
 def sin_tilde(nombre_calle):
     return nombre_calle.translate(str.maketrans("ÁÉÍÓÚ", "AEIOU"))
 
-def sin_tilde(nombre_calle):
-    return nombre_calle.translate(str.maketrans("ÁÉÍÓÚ", "AEIOU"))
 
-
-def search_calle(nombre_calle=None):
+def calle_txt(nombre_calle=None):
     file_txt = ruta
     calle_info = []
 
     print(nombre_calle)
     nombre_calle = (' ').join(nombre_calle.split('_'))
-    print('Despues Split', nombre_calle)
+    print('Despues Split y Strip', nombre_calle.strip())
     nombre_calle = f'{nombre_calle.upper()}'
     print("Despues de (calle)", nombre_calle)
     nombre_calle = sin_tilde(nombre_calle)
@@ -32,20 +34,16 @@ def search_calle(nombre_calle=None):
 
     #(CRAMER \(calle\)|CRAMER \(avenida\))
     with open(file_txt, encoding='latin1') as openfile:
-        index_1 = 0
         for linea in openfile:
 
-            if index_1 == 1:
-                #print("LINEA >", linea)
-                linea = linea.replace('- ', '')
-                linea = linea.replace('', '"').replace('', '"')
-                calle_info.append(linea.strip())
-                break
-
             if pattern.search(linea) is not None:
-                index_1 = 1
+                #print(linea)
+                calle_info.append(linea)
 
     if calle_info:
         return calle_info[0]
     print(calle_info)
     return calle_info
+
+
+# print(calle_txt(nombre_calle))
